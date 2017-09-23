8f306517f6134b58277b869b330e39294cc530ca
Timothy Thompsen <TimmyThompsen@Timothys-MacBook-Pro.local>
2017-09-23 13:28:00 -0700

    Making progress

--- assets/javascript/firebasemap.js
+++ assets/javascript/firebasemap.js
@@ -24,6 +24,7 @@ function initMap() {
   var map = new google.maps.Map(document.getElementById('map'), {
     center: {lat: 32.823, lng: -117.283},
     zoom: 10,
+    gestureHandling: 'greedy',
     styles: [{
       featureType: 'poi',
       stylers: [{ visibility: 'off' }]  // Turn off points of interest.
@@ -33,6 +34,7 @@ function initMap() {
     }],
     disableDoubleClickZoom: true
   });
+
 
    // Add markers to the map.
     database.ref("/spitCast").on("child_added", function(childSnapshot, prevChildKey) {
@@ -154,8 +156,8 @@ function initMap() {
                         dir="NNW";
                       }
 
-                      var displayBox = '<div id="infoWindow"><p>'
-                      + response[0].spot_name +'</p><p>Morning Conditions: '
+                      var displayBox = '<div id="infoWindow"><h3>'
+                      + response[0].spot_name +'</h3><p>Morning Conditions: '
                       + response[5].size+'ft' + '</br> Shape: ' + response[5].shape_full
                       + '</br> Swell Direction: '+ dir + '</br> Wind: ' + response3[5].speed_kts + '</br> Tide: ' + response4[5].tide_meters + '</br>'
                       +'</p><p>Midday Conditions: ' 
@@ -183,13 +185,33 @@ function initMap() {
         spotNum = marker.idNum;
         display();
         console.log("marker clicked");
-        
-        //closes infoWindow2 on mouseover so that infoWindow1 will stay open until clicked again.
         infoWindow2.close(map, marker);           
       });
       
       document.getElementById('search').onclick = function() {
-    // do magic
+        var searchValue = document.getElementById("searchval").value.trim();
+        console.log(searchValue);
+        var temp = searchValue.toLowerCase();
+        var queryURL = "http://api.spitcast.com/api/county/spots/san-diego/";
+        //ajax call
+          $.ajax({
+            url: queryURL,
+            method: "GET"
+          })
+          .done(function(response){
+            console.log(response);
+            var found = false;
+            for(var i=0; i < response.length; i++){
+              if(temp === response[i].spot_name.toLowerCase().trim()){
+                spotNum = response[i].spot_id;
+                display();
+                found = true;
+            }
+          }
+            if(found === false){
+              alert("No results found.");
+            }
+          });
       };
 
       // mouse over funtions
@@ -204,82 +226,6 @@ function initMap() {
         infoWindow2.close(map, marker);
       });
     });
-    
-      //   //testing google places api
-      // // Create the search box and link it to the UI element.
-      //   // Create the search box and link it to the UI element.
-      //   var input = document.getElementById('pac-input');
-      //   var searchBox = new google.maps.places.SearchBox(input);
-      //   map.controls[google.maps.ControlPosition.TOP_LEFT].push(input);
-
-      //   // Bias the SearchBox results towards current map's viewport.
-      //   map.addListener('bounds_changed', function() {
-      //     searchBox.setBounds(map.getBounds());
-      //   });
-
-      //   var markers = [];
-      //   // Listen for the event fired when the user selects a prediction and retrieve
-      //   // more details for that place.
-      //   searchBox.addListener('places_changed', function() {
-      //     var places = searchBox.getPlaces();
-
-      //     if (places.length == 0) {
-      //       return;
-      //     }
-
-      //     // Clear out the old markers.
-      //     markers.forEach(function(marker) {
-      //       marker.setMap(null);
-      //     });
-      //     markers = [];
-
-      //     // For each place, get the icon, name and location.
-      //     var bounds = new google.maps.LatLngBounds();
-      //     places.forEach(function(place) {
-      //       if (!place.geometry) {
-      //         console.log("Returned place contains no geometry");
-      //         return;
-      //       }
-      //       var icon = {
-      //         url: place.icon,
-      //         size: new google.maps.Size(71, 71),
-      //         origin: new google.maps.Point(0, 0),
-      //         anchor: new google.maps.Point(17, 34),
-      //         scaledSize: new google.maps.Size(25, 25)
-      //       };
-
-      //       // Create a marker for each place.
-      //       markers.push(new google.maps.Marker({
-      //         map: map,
-      //         icon: icon,
-      //         title: place.name,
-      //         position: place.geometry.location
-      //       }));
-
-      //       if (place.geometry.viewport) {
-      //         // Only geocodes have viewport.
-      //         bounds.union(place.geometry.viewport);
-      //       } else {
-      //         bounds.extend(place.geometry.location);
-      //       }
-      //     });
-      //     map.fitBounds(bounds);
-      //   });
-
-
- 
-
-
-    
-   // Add a marker clusterer to manage the markers.
-   // var markerCluster = new MarkerClusterer(map, marker,
-   //     {imagePath: 'https://developers.google.com/maps/documentation/javascript/examples/markerclusterer/m'});
-   
-   // Listen for click event
-   // Store click location to firebase
-    /*map.addListener('click', function(e) {
-    placeMarker(e.latLng, map);
-    });*/
 
   function placeMarker(position, map) {
     var marker = new google.maps.Marker({
@@ -295,35 +241,32 @@ function initMap() {
     }
     database.ref("/userSpots").push(objPosition);
   }
-
 }
 
-  var locations = [
-    {lat: -31.563910, lng: 147.154312},
-    {lat: -33.718234, lng: 150.363181},
-    {lat: -33.727111, lng: 150.371124},
-    {lat: -33.848588, lng: 151.209834},
-    {lat: -33.851702, lng: 151.216968},
-    {lat: -34.671264, lng: 150.863657},
-    {lat: -35.304724, lng: 148.662905},
-    {lat: -36.817685, lng: 175.699196},
-    {lat: -36.828611, lng: 175.790222},
-    {lat: -37.750000, lng: 145.116667},
-    {lat: -37.759859, lng: 145.128708},
-    {lat: -37.765015, lng: 145.133858},
-    {lat: -37.770104, lng: 145.143299},
-    {lat: -37.773700, lng: 145.145187},
-    {lat: -37.774785, lng: 145.137978},
-    {lat: -37.819616, lng: 144.968119},
-    {lat: -38.330766, lng: 144.695692},
-    {lat: -39.927193, lng: 175.053218},
-    {lat: -41.330162, lng: 174.865694},
-    {lat: -42.734358, lng: 147.439506},
-    {lat: -42.734358, lng: 147.501315},
-    {lat: -42.735258, lng: 147.438000},
-    {lat: -43.999792, lng: 170.463352},
-    {lat: 33.034405, lng: -117.292928},
-    {lat: 32.034405, lng: -117.292928},
-    {lat: 33.034405, lng: -118.292928},
-    {lat: 34.034405, lng: -117.292928}
-  ]
+/******/
+ //Login Modal Function
+ /******/
+ // Get the modal
+var modal = document.getElementById('myModal');
+// Get the button that opens the modal
+var btn = document.getElementById("myBtn");
+// Get the <span> element that closes the modal
+var span = document.getElementsByClassName("close")[0];
+// When the user clicks the button, open the modal 
+loginBtn.onclick = function() {
+    modal.style.display = "block";
+}
+// When the user clicks on <span> (x), close the modal
+$(".close").on('click', function(){
+  modal.style.display = "none";
+})
+// span.onclick = function() {
+//     modal.style.display = "none";
+// }
+// When the user clicks anywhere outside of the modal, close it
+window.onclick = function(event) {
+    if (event.target == modal) {
+        modal.style.display = "none";
+    }
+}
+
