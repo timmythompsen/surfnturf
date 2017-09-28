3fc683a190afdf34226b02532738181ca59f62cb
jbhafner <brian_hafner@yahoo.com>
2017-09-28 14:33:43 -0700

    re-fixes the modals and buttons

--- assets/javascript/auth.js
+++ assets/javascript/auth.js
@@ -57,7 +57,7 @@ window.onload=(function() {
             displayName: dispName
         }).then(function() {
             // Update successful.
-            // $("#loggedInAs").html("Logged in as: " + user.displayName + "   ");
+            $("#loggedInAs").html("Logged in as: " + user.displayName + "   ");
         }, function(error) {
             // An error happened.
         });        
@@ -104,9 +104,11 @@ window.onload=(function() {
       if(firebaseUser.displayName === undefined || firebaseUser.displayName === null) {
         return;
       } else {
+      $("#loggedInAs").html("Logged in as: " + firebaseUser.displayName + "   ");
 
-        var loggedIn = $("#nbBtnLogin");
-        $(loggedIn).replaceWith("<button class='btn btn-outline-success my-2 my-sm-0 btn-static'>" + "<span class='glyphicon glyphicon-tint blue'></span>" + "Username: " + firebaseUser.displayName + "</button>");
+
+        // var loggedIn = $("#nbBtnLogin");
+        // $(loggedIn).replaceWith("<button class='btn btn-outline-success my-2 my-sm-0 btn-static'>" + "<span class='glyphicon glyphicon-tint blue'></span>" + "Username: " + firebaseUser.displayName + "</button>");
       }
     } else {
       console.log('not logged in'); 
@@ -119,17 +121,17 @@ window.onload=(function() {
 
 
 // FOR TESTING -- CAN BE DELETED ONCE LIVE -- IT IS OUTSIDE THE MAIN FUNCTION 
-  btnPrintUser.addEventListener('click',e=> {
-  var user = firebase.auth().currentUser;
-  if (user != null) {
-      user.providerData.forEach(function (profile) {
-      console.log("Sign-in provider: "+profile.providerId);
-      console.log("  Provider-specific UID: "+profile.uid);
-      console.log("  Name: "+profile.displayName);
-      console.log("  Email: "+profile.email);
-      console.log("  Photo URL: "+profile.photoURL);
-      console.log("  uid: "+profile.uid);
-      console.log (profile);
-    });
-  }
-});
+//   btnPrintUser.addEventListener('click',e=> {
+//   var user = firebase.auth().currentUser;
+//   if (user != null) {
+//       user.providerData.forEach(function (profile) {
+//       console.log("Sign-in provider: "+profile.providerId);
+//       console.log("  Provider-specific UID: "+profile.uid);
+//       console.log("  Name: "+profile.displayName);
+//       console.log("  Email: "+profile.email);
+//       console.log("  Photo URL: "+profile.photoURL);
+//       console.log("  uid: "+profile.uid);
+//       console.log (profile);
+//     });
+//   }
+// });
